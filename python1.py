a29ea724233a80892c4575094749fc8444f2b003
gsanchez13 <gisellesanchez@pursuit.org>
2020-08-02 12:28:36 -0400

    strings

--- python1.py
+++ python1.py
@@ -8,5 +8,17 @@ discount = 0.2
 result = price * (1 - discount)
 #follows PEMDAS 
 #functions perform actions or calculate options
-print(result)
-#
+# print(result)
+
+#STRINGS
+name = "Rolf"
+# print(name)
+# print(name * 2)
+# Will concatinate the stirngs based on the number of times you want it to appear. Strings can be multiplied in python
+a = 25
+b = a
+b = 17
+#same variable being reassigned
+# print(a)
+# print(b)
+#mutability in the languages will allow you to change the original ariable (a) using another variable (b)
