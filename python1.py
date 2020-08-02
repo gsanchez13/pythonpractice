9a1415e81f2eba93de6190514d2e17ffcb54ae36
gsanchez13 <gisellesanchez@pursuit.org>
2020-08-02 12:18:38 -0400

    first commit

---
+++ python1.py
@@ -0,0 +1,5 @@
+x = 15
+# right side happens first and left side happens next
+# stored in memory to the variable
+price = 9.99
+# integers are whole numbers and floats are numbers with a floating/decimal point
